e917c2dd0644ea03bdd2637151acec6f0186c710
tungcuong <tungcuong131@gmail.com>
2024-02-17 17:57:39 +0700

    init

---
+++ dart/3.collections/collection.dart
@@ -0,0 +1,16 @@
+// Collections in dart support: list, set, map
+
+void testList() {
+  var lists = [1, 2, 3, 4];
+  lists.add(5);
+  print("lists = $lists");
+}
+
+void testSet() {}
+
+void testMap() {}
+void main(List<String> args) {
+  testList();
+  testSet();
+  testMap();
+}
